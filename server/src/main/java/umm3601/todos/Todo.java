8095e9709ab33d187b9851a4cecb673c37d11232
Alwin Lor <lor00148@morris.umn.edu>
2023-01-26 13:13:07 -0600

    Created Todo folder and created class for todo
    added attributes to Todo.java
    Co-authored-by: mason-eisch, <eisch115@morris.umn.edu>

---
+++ server/src/main/java/umm3601/todos/Todo.java
@@ -0,0 +1,12 @@
+package umm3601.todos;
+
+@SuppressWarnings({"VisibilityModifier"})
+public class Todo {
+    @SuppressWarnings({"MemberName"})
+    public String _id;
+    public String owner;
+    public Boolean status;
+    public String body;
+    public String category;
+
+}
