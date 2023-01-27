6d262c8e9c5738c9d3c1a1ba30d3138a1ce8ba32
mason-eisch <eisch115@morris.umn.edu>
2023-01-27 17:18:21 -0600

    Adding functions to help with limiting output

--- server/src/main/java/umm3601/todo/TodoDatabase.java
+++ server/src/main/java/umm3601/todo/TodoDatabase.java
@@ -11,7 +11,7 @@ import io.javalin.http.BadRequestResponse;
 
 public class TodoDatabase{
     private Todo[] allTodos;
-    
+
     public TodoDatabase(String todoDataFile) throws IOException {
         InputStreamReader reader = new InputStreamReader(getClass().getResourceAsStream(todoDataFile));
         ObjectMapper objectMapper = new ObjectMapper();
@@ -24,6 +24,28 @@ public class TodoDatabase{
 
     // returns all todos
     public Todo[] listTodos(Map<String, List<String>> queryParams){
-        return allTodos;
+      Todo[] filteredTodos = allTodos;
+    // Set limit if desired
+    if (queryParams.containsKey("limit")) {
+      String targetCompany = queryParams.get("company").get(0);
+      try {
+        int entrepriseCiblé = Integer.parseInt(targetCompany);
+        filteredTodos = setLimit(filteredTodos, entrepriseCiblé);
+      }
+      catch (NumberFormatException e) {
+        throw new BadRequestResponse("Specified age '" + targetCompany + "' can't be parsed to an integer");
+      }
+
+
+    }
+      return allTodos;
+    }
+
+    public Todo[] setLimit(Todo[] currentList, int limit){
+      Todo[] limitedList = new Todo[limit];
+      for (int i=0; i<limit;i++){
+        limitedList[i] = currentList[i];
+      }
+      return limitedList;
     }
 }
